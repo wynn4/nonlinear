eb16ba5e120ff65a91b58f7f8ad29979a4e08a88
Jesse Wynn <wynnsquad@gmail.com>
2018-03-08 15:08:16 -0700

    added examples

---
+++ midterm/find_eq.m
@@ -0,0 +1,22 @@
+clc
+clear
+close all
+
+% solve for the equilibria of a system
+
+% declare symbolic variabls
+syms x1 x2
+
+% define system of equations we'd like to solve
+% notice == 0 because we're looking for equilibirum (xdot = 0)
+% here we do HK problem 2.1(2) from the hw as an exmaple
+eqns = [x1 + x1*x2 == 0, -x2 + x2^2 + x1*x2 - x1^3 == 0];
+vars = [x1, x2];
+[solv, solu] = solve(eqns, vars);
+
+% display the solutions
+solutions = [solv, solu]
+
+% Note solv corresponds to x1 values solu corresponds to x2 values.
+% Ignore any solutions with imaginary parts (can't have imaginary
+% equilibria for real systems).
---
+++ midterm/phase_plot.m
@@ -0,0 +1,77 @@
+clc
+clear
+close all
+
+% define number of grid points
+N = 10;
+
+% define our function of differential equaitons
+% f @(t, X) [X1dot; X2dot];
+f = @(t, X) [X(2); -X(1) + (1/16)*(X(1)^5) - X(2)];
+
+% create a grid where we will evaluate the function derivatives
+x1 = linspace(-3, 3, N);  % from -5 to 5 on the x1 axis with N points
+x2 = linspace(-1, 1, N);  % same^
+
+% create a meshgrid (two NxN matrices), one for all the x values on the
+% grid and one for all the y values on the grid
+[x,y] = meshgrid(x1,x2);
+
+% create u matrix to hold values of X1dot at each x and y position in the
+% grid
+u = zeros(size(x));
+% create v matrix to hold values of X2dot at each x and y position in the
+% grid
+v = zeros(size(y));
+
+% evaluate the Xdot at each point in the grid
+t=0;  
+for i = 1:numel(x)
+    Xdot = f(t, [x(i); y(i)]);
+    u(i) = Xdot(1);
+    v(i) = Xdot(2);
+end
+
+% u = u*100;
+% v = v*100;
+
+% plot phase plot using quiver
+figure(1), clf
+quiver(x,y,u,v,'r', 'AutoScaleFactor', 2.0);
+xlabel('x1')
+ylabel('x2')
+title('phase plot')
+axis tight equal;
+
+
+% plot solutions on plot
+hold on
+X0 = [1.95, 0.5]';  % define initial x1 and x2 locations
+tspan = [0, 1];  % lets see solutions from time t = 0 to 30
+
+% solve using ode45
+[ts, xs] = ode45(f, tspan, X0);
+plot(xs(:,1), xs(:,2), 'b')
+plot(xs(1,1),xs(1,2),'bo') % starting point
+plot(xs(end,1),xs(end,2),'ks') % ending point
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
